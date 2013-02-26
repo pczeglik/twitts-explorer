38871982ccd98ecb0be190f24362b5e9a948417b
Piotr Czeglik <pczeglik@gmail.com>
2013-02-26 22:43:58 +0100

    base css, handlebars and underscore

--- twitter-explorer/app/main.js
+++ twitter-explorer/app/main.js
@@ -1,17 +1,29 @@
 require.config({
   baseUrl: '../twitter-explorer/',
   paths: {
-    'jquery': 'lib/jquery'
+    'jquery': 'lib/jquery',
+    'underscore': 'lib/underscore',
+    'handlebars': 'lib/handlebars'
   }
 });
 
-define(['app/twitts.explorer'], function (TwittsExplorer) {
-  var tw = new TwittsExplorer();
+define(['app/twitts.explorer', 'handlebars'], function (TwittsExplorer, Handlebars) {
+  var tw = new TwittsExplorer(),
+    $searchInput = $('#search-phrase'),
+    $searchSubmit = $('#search-submit'),
+    $twitts = $('.twitts'),
+    $template = $('#twitts');
 
-  $('#search-submit').on('click', function () {
-    var phrase = $('#search-phrase').val();
-    tw.getTwitts(phrase).then(function (results) {
-      console.log(results);
-    });
+  $searchSubmit.on('click', function () {
+    var phrase = $searchInput.val();
+    if (phrase && phrase !== '') {
+      tw.getTwitts(phrase, function (twitts) {
+        var template = Handlebars.compile($template.html()),
+          html = template({'twitts': twitts});
+        $twitts.append(html);
+      });
+    } else {
+      $searchInput.addClass('error').focus();
+    }
   });
 });
--- twitter-explorer/app/twitts.explorer.js
+++ twitter-explorer/app/twitts.explorer.js
@@ -1,4 +1,8 @@
+/*global define */
+
 define(['jquery'], function ($) {
+  "use strict";
+
   var defaults = {
       twitterApiUrl: 'http://search.twitter.com/search.json'
     },
@@ -14,20 +18,47 @@ define(['jquery'], function ($) {
      * @param  {object} options configuration options
      */
     twittsExplorer.prototype.init = function (options) {
-      this.config = $.extend({}, defaults, options);
+      this.config = typeof options === 'object' ?
+        $.extend({}, defaults, options) :
+        $.extend({}, defaults);
     };
 
     /**
      * Get twitts by phrase
      * @param  {string} phrase phrase to look for
+     * @param {function} callback
      * @return {jQuery Deferred}
      */
-    twittsExplorer.prototype.getTwitts = function (phrase) {
+    twittsExplorer.prototype.getTwitts = function (phrase, callback) {
+      var self = this;
+      this.askApi(phrase).then(function (results) {
+        var twitts = [];
+
+        for (var i = 0, j = results.length; i < j; i += 1) {
+          twitts.push(self.mapTwitt(results[i]));
+        }
+        if (typeof callback === 'function') {
+          callback(twitts);
+        } else {
+          return twitts;
+        }
+      });
+    };
+
+    twittsExplorer.prototype.askApi = function (phrase) {
       var dfd = $.Deferred();
-      $.getJSON(this.config.twitterApiUrl + '?q='+ phrase + '&callback=?', function (result) {
+      $.getJSON(this.config.twitterApiUrl + '?q=' + phrase + '&callback=?', function (result) {
         dfd.resolve(result.results);
       });
       return dfd.promise();
+    };
+
+    twittsExplorer.prototype.mapTwitt = function (twitt) {
+      return {
+        text: twitt.text,
+        from: twitt.from_user_name,
+        to: twitt.to_user_name
+      }
     };
 
   return twittsExplorer;
