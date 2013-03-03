24b1297623b43ee97281dedecb70589f466fe490
Piotr Czeglik <pczeglik@gmail.com>
2013-03-03 17:57:45 +0100

    Added ommited files

--- twitter-explorer/app/main.js
+++ twitter-explorer/app/main.js
@@ -14,16 +14,26 @@ define(['app/twitts.explorer', 'handlebars'], function (TwittsExplorer, Handleba
     $twitts = $('.twitts'),
     $template = $('#twitts');
 
-  $searchSubmit.on('click', function () {
-    var phrase = $searchInput.val();
+  var doSearch = function doSearch(phrase) {
     if (phrase && phrase !== '') {
       tw.getTwitts(phrase, function (twitts) {
-        var template = Handlebars.compile($template.html()),
+        var template = Handlebars.compile($template.html(), {noEscape: true}),
           html = template({'twitts': twitts});
         $twitts.append(html);
       });
     } else {
       $searchInput.addClass('error').focus();
     }
+  };
+
+  $searchSubmit.on('click', function () {
+    doSearch($searchInput.val());
   });
+
+  $searchInput.on('keypress', function (e) {
+    if (e.which === 13) {
+      doSearch($searchInput.val());
+    }
+  });
+
 });
--- twitter-explorer/app/twitts.explorer.js
+++ twitter-explorer/app/twitts.explorer.js
@@ -10,6 +10,7 @@ define(['jquery'], function ($) {
      * @constructor
      */
     twittsExplorer = function twittsExplorer() {
+      this.index = 1;
       this.init();
     };
 
@@ -30,12 +31,18 @@ define(['jquery'], function ($) {
      * @return {jQuery Deferred}
      */
     twittsExplorer.prototype.getTwitts = function (phrase, callback) {
-      var self = this;
-      this.askApi(phrase).then(function (results) {
+      var self = this,
+        options = {
+          phrase: phrase,
+          page: 1,
+          rpp: 10
+        };
+
+      this.askApi(options).then(function (results) {
         var twitts = [];
 
         for (var i = 0, j = results.length; i < j; i += 1) {
-          twitts.push(self.mapTwitt(results[i]));
+          twitts.push(self.mapTwitt(results[i], options.page - 1));
         }
         if (typeof callback === 'function') {
           callback(twitts);
@@ -45,20 +52,30 @@ define(['jquery'], function ($) {
       });
     };
 
-    twittsExplorer.prototype.askApi = function (phrase) {
+    twittsExplorer.prototype.askApi = function (options) {
       var dfd = $.Deferred();
-      $.getJSON(this.config.twitterApiUrl + '?q=' + phrase + '&callback=?', function (result) {
+      $.getJSON(this.config.twitterApiUrl + '?q=' + options.phrase + '&page=' + options.page + '&rpp=' + options.rpp + '&callback=?', function (result) {
         dfd.resolve(result.results);
+        options.page += 1;
       });
       return dfd.promise();
     };
 
-    twittsExplorer.prototype.mapTwitt = function (twitt) {
+    twittsExplorer.prototype.mapTwitt = function (twitt, page) {
+      var self = this;
       return {
-        text: twitt.text,
+        idx: page * 10 + self.index++,
+        text: self.convertLinks(twitt.text),
         from: twitt.from_user_name,
         to: twitt.to_user_name
       }
+    };
+
+    twittsExplorer.prototype.convertLinks = function (text) {
+      var re = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+      return text.replace(re, function ($1, $2, $3) {
+        return '<a href="' + $1 + '">' + $1 + '</a>';
+      });
     };
 
   return twittsExplorer;
